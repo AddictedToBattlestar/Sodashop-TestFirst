17ebc89fe76af6506270151abf758cc7f7be6890
John Ryan <john.ryan@cardinalhealth.com>
2016-05-11 20:08:15 -0400

    Going logic up toward root so that I can build a consolidated header

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import Config from '../config/environment';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  notify: Ember.inject.service('notify'),
+  notificationCloseAfter: Config.notificationCloseAfter,
+  amountInserted: 0,
+  alternateMessageToDisplay: '',
   actions: {
     returnToHome() {
         this.transitionToRoute('sodaBrands.sodaBrand.sodas', 1);
@@ -12,6 +17,35 @@ export default Ember.Controller.extend({
       },
       login() {
         this.transitionToRoute('login');
+      },
+      generateNotification(notificationType, message, options) {
+        if (!options) {
+          options = {
+            closeAfter: this.get('notificationCloseAfter')
+          };
+        }
+        let notify = this.get('notify');
+        if (notificationType === 'error') {
+          notify.error(message, options);
+        } else if (notificationType === 'warning') {
+          notify.warning(message, options);
+        } else if (notificationType === 'success') {
+          notify.success(message, options);
+        } else if (notificationType === 'raw') {
+          notify.success(window.$.extend(options, {
+            html: message
+          }));
+        }
+      },
+      displayTemporaryMessage(temporaryMessage) {
+        this.set('alternateMessageToDisplay', temporaryMessage);
+        let that = this;
+        let timeout = Config.notificationCloseAfter;
+        if (timeout) {
+          Ember.run.later((function() {
+            that.set('alternateMessageToDisplay', '');
+          }), timeout);
+        }
       }
   }
 });
--- app/controllers/soda-brands.js
+++ app/controllers/soda-brands.js
@@ -1,45 +1,11 @@
-import Config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
   applicationModel: Ember.computed.reads('applicationController.model'),
-  notify: Ember.inject.service('notify'),
-  notificationCloseAfter: Config.notificationCloseAfter,
-  amountInserted: 0,
-  alternateMessageToDisplay: '',
   actions: {
     transitionToSodaBrand(sodaBrand) {
         this.transitionToRoute('sodaBrands.sodaBrand.sodas', sodaBrand.id);
-      },
-      generateNotification(notificationType, message, options) {
-        if (!options) {
-          options = {
-            closeAfter: this.get('notificationCloseAfter')
-          };
-        }
-        let notify = this.get('notify');
-        if (notificationType === 'error') {
-          notify.error(message, options);
-        } else if (notificationType === 'warning') {
-          notify.warning(message, options);
-        } else if (notificationType === 'success') {
-          notify.success(message, options);
-        } else if (notificationType === 'raw') {
-          notify.success(window.$.extend(options, {
-            html: message
-          }));
-        }
-      },
-      displayTemporaryMessage(temporaryMessage) {
-        this.set('alternateMessageToDisplay', temporaryMessage);
-        let that = this;
-        let timeout = Config.notificationCloseAfter;
-        if (timeout) {
-          Ember.run.later((function() {
-            that.set('alternateMessageToDisplay', '');
-          }), timeout);
-        }
       }
   }
 });
